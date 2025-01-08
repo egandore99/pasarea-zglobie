ec750ca432c6be844cf6e5a12a967ad52cee821d
Eduard <eduardgandore99@gmail.com>
2025-01-08 18:34:43 +0200

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private int score;
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over"); 
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+    }
+
+}
